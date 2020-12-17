fdc630a3ed03f44acadbddf82842a759c92fa528
clichyclin <clichyclin@gmail.com>
2020-12-17 20:23:13 +0000

    ...

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,10 +3,9 @@ def square(x):
 return x * x
 
 def square_matrix_simple(matrix=[]):
+    new_list = matrix[:]
 
-new_list = matrix[:]
+    for i in range(len(new_list)):
+    new_list[i] = map(square, new_list[i])
 
-for i in range(len(new_list)):
-new_list[i] = map(square, new_list[i])
-
-return new_list
+    return new_list
