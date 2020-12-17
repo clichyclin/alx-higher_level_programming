76c2183649b0ea77bbe7e8860725032debddd246
clichyclin <clichyclin@gmail.com>
2020-12-17 20:26:53 +0000

    ...

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    n_matrix = []
+n_matrix = []
 
-    for row in matrix:
-        n_matrix.append(list(map(lambda row: row**2, row)))
-
-    return (n_matrix)
+for row in matrix:
+n_matrix.append(list(map(lambda row: row**2, row)))
+return (n_matrix)
